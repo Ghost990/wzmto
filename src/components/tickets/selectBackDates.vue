1fc3b63fddfe6913ccfec69b043c887054bceb0c
Zsolt Nagy <ghost990@gmail.com>
2018-02-06 14:40:09 +0100

    refactored selectBackDate, added animations and conditional showing of the Show Flights button

--- src/components/tickets/selectBackDates.vue
+++ src/components/tickets/selectBackDates.vue
@@ -5,13 +5,12 @@
         <div class="col-12 col-sm-8 select-text">
           <h3>Would You like to buy ticket for return?</h3>
           <p>Select a return date from the date selecor.</p>
-          <p>retdate:{{ departureDateSelected }}</p>
         </div>
         <div class="col-12 col-sm-4 select-button">
           <button v-if="!isShowingDateselect" class="select-dates-trigger" @click="selectReturnDate">
             Select date
           </button>
-          <div class="input-group return-date-select" v-if="isShowingDateselect">
+          <div class="input-group return-date-select" v-animate-css="'fadeIn'" v-if="isShowingDateselect">
             <flat-pickr
               v-model="returnDate"
               :config="configReturn"
@@ -19,15 +18,16 @@
               :placeholder="'Select Return Date'"
               name="date"
               :disabled="false"
+              @on-change="dateChanged"
             >
             </flat-pickr>
           </div>
         </div>
       </div>
-      <div class="row">
+      <div class="row" v-if="isReturnNeeded" v-animate-css="'fadeIn'">
         <div class="col-12 col-sm-6 offset-sm-3">
-          <button class="wizz-button wizz-button-primary" @click="getReturnFlights">
-            Get
+          <button class="wizz-button wizz-button-primary rounded show-flights" @click="getReturnFlights">
+            Show Flights
           </button>
         </div>
       </div>
@@ -36,7 +36,6 @@
 </template>
 
 <script>
-  import axios from 'axios';
   import moment from 'moment';
   import { bus } from '../../main';
 
@@ -45,16 +44,8 @@
       return {
         returnDate: '',
         departureDateSelected: '',
-        isTicketsShow: false,
         isReturnNeeded: false,
         isShowingDateselect: false,
-        returnActualFlight: [],
-        returnDepartureCity: '',
-        returnDestinationCity: '',
-        returnSelectedDate: '',
-        returnDepartureIata: '',
-        returnDestinationIata: '',
-        returnSelectedTicket: '',
         configReturn: {
           wrap: true, // set wrap to true only when using 'input-group'
           altFormat: 'l, J F Y',
@@ -63,29 +54,23 @@
         }
       }
     },
-    watch: {
-      departureDate() {
-        this.selectReturnDate();
-      }
-    },
     methods: {
       selectReturnDate() {
         this.isShowingDateselect = true;
         this.returnDate = moment(this.departureDateSelected, 'YYYY-MM-DD').add(1, 'day').format('YYYY-MM-DD');
         this.configReturn = { minDate: this.returnDate };
-        this.isReturnNeeded = true;
       },
       getReturnFlights(url) {
         bus.$emit('selectbackdate', this.departureDateSelected);
       },
+      dateChanged() {
+        this.isReturnNeeded = true;
+      }
     },
     created() {
       bus.$on('returnselecteddate', (event) => {
         this.departureDateSelected = event;
       });
-
-      console.log(this.departureDateSelected);
-
     }
   }
 </script>
